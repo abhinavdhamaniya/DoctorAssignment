5704f0ef372d8e8900a5f64e853f2262f5584e6e
abhinavdhamaniya <abhinavdhamaniya@gmail.com>
2023-02-04 00:25:40 +0530

    Added Team and Player Assignment

--- src/main/java/com/abhinav/TeamAndPlayerAssignment/dao/PlayerDAO.java
+++ src/main/java/com/abhinav/TeamAndPlayerAssignment/dao/PlayerDAO.java
@@ -1,9 +1,7 @@
 package com.abhinav.TeamAndPlayerAssignment.dao;
 
 import com.abhinav.TeamAndPlayerAssignment.entities.Player;
-import com.abhinav.TeamAndPlayerAssignment.entities.Team;
 import com.abhinav.TeamAndPlayerAssignment.repository.PlayerRepository;
-import com.abhinav.TeamAndPlayerAssignment.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
